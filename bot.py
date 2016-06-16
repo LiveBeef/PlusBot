6e8eccdc431dabba71fd8ca644f2915798f57900
captainmeta4 <Adam@Adams-MacBook-Pro.local>
2016-06-16 14:30:26 -0400

    Generalize bot for multiple subreddits

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ import os
 
 #Set globals
 r=praw.Reddit("/r/AutoModerator Contribution Bot")
-sub = r.get_subreddit("AutoModerator")
+sub = r.get_subreddit('captainmeta4bots')
 
 
 class Bot():
@@ -60,17 +60,17 @@ class Bot():
 
     def scan_comments(self):
 
-        for comment in praw.helpers.comment_stream(r, "AutoModerator", limit=100, verbosity=0):
+        for comment in praw.helpers.comment_stream(r, "mod", limit=100, verbosity=0):
 
             #Flair reset
             if comment.author_flair_css_class == "reset":
                 comment.remove()
                 score = 0
                 if comment.author.name in self.author_points:
-                    score = len(self.author_points[comment.author.name])
+                    score = len(self.author_points[comment.subreddit.display_name][comment.author.name])
                 flair_class = self.score_class(score)
                 flair_text = "+"+str(score)
-                r.set_flair(sub, comment.author, flair_text=flair_text, flair_css_class = flair_class)
+                r.set_flair(comment.subreddit, comment.author, flair_text=flair_text, flair_css_class = flair_class)
                 continue
 
 
@@ -99,54 +99,36 @@ class Bot():
                 continue
 
             #add user to authorpoints
-            if parent_comment.author.name not in self.author_points:
-                self.author_points[parent_comment.author.name]=[]
+
+            if comment.subreddit.display_name not in self.author_points:
+                self.author_points[comment.subreddit.display_name]={}
+            if parent_comment.author.name not in self.author_points[comment.subreddit.display_name]:
+                self.author_points[comment.subreddit.display_name][parent_comment.author.name]=[]
 
             #check to see if user has scored this thread
-            if comment.link_id in self.author_points[parent_comment.author.name]:
+            if comment.link_id in self.author_points[comment.subreddit.display_name][parent_comment.author.name]:
                 continue
 
             #add user to authorpoints
-            self.author_points[parent_comment.author.name].append(comment.link_id)
+            self.author_points[comment.subreddit.display_name][parent_comment.author.name].append(comment.link_id)
 
             #get new score and flair text
-            score = len(self.author_points[parent_comment.author.name])
-            text = "+"+str(score)
+            score = len(self.author_points[comment.subreddit.display_name][parent_comment.author.name])
 
 
             #variables for new flair
             flair_class = self.score_class(score)
-            flair_text = text
+            flair_text = "+"+str(score)
 
 
-            #if user has special flair, preserve it and the text. Otherwise set flair class by score.
+            #if user has no flair, or score flair, set new score flair
             if parent_comment.author_flair_css_class is None:
-                pass
-            
-            elif "contributor" in parent_comment.author_flair_css_class:
-                flair_text = "Contributor "+text
-                flair_class = "contributor"
+                #save flair to reddit
+                r.set_flair(comment.subreddit,parent_comment.author,flair_text = flair_text, flair_css_class = flair_class)
 
-            elif "regexninja" in parent_comment.author_flair_css_class:
-                flair_text = "Regex Ninja "+text
-                flair_class = "regexninja"
-
-            elif "sorcerer" in parent_comment.author_flair_css_class:
-                flair_text = "Open Sorcerer "+text
-                flair_class = "sorcerer"
-
-            elif "user" in parent_comment.author_flair_css_class:
-                #certain unique flairs are passed on
-                flair_text = parent_comment.author_flair_text
-                flair_class = parent_comment.author_flair_css_class
-                
-            elif "plain" in parent_comment.author_flair_css_class:
-                flair_text = parent_comment.author_flair_text
-                flair_class = "plain"
-
-
-            #set new flair text and save to reddit flair_class}
-            r.set_flair(sub,parent_comment.author,flair_text = flair_text, flair_css_class = flair_class)
+            elif any(x in parent_comment.author_flair_css_class for x in ['score-t1','score-t2','score-t3','score-t4']):
+                #save flair to reddit
+                r.set_flair(comment.subreddit,parent_comment.author,flair_text = flair_text, flair_css_class = flair_class)
 
             #save new authorpoints to wiki
             reason = parent_comment.author.name+" "+comment.link_id+" "+text
